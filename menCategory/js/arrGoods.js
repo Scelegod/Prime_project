d52e3f58720297c43313cc0e8007153b089848cc
Scelegod <thescelegod@mail.ru>
2023-03-31 10:50:05 +0300

    rework

--- menCategory/js/arrGoods.js
+++ menCategory/js/arrGoods.js
@@ -8,6 +8,16 @@ let btnHomclot = document.querySelector('.btnHomclot');
 let btnShoes = document.querySelector('.btnShoes');
 let btnNewModels = document.querySelector('.btnNewModels');
 let btnDiscounts = document.querySelector('.btnDiscounts');
+
+let sidenav__btnTshirts = document.querySelector('.sidenav__btnTshirts');
+let sidenav__btnPants = document.querySelector('.sidenav__btnPants');
+let sidenav__btnJacket = document.querySelector('.sidenav__btnJacket');
+let sidenav__btnHoodies = document.querySelector('.sidenav__btnHoodies');
+let sidenav__btnJeans = document.querySelector('.sidenav__btnJeans');
+let sidenav__btnHomclot = document.querySelector('.sidenav__btnHomclot');
+let sidenav__btnShoes = document.querySelector('.sidenav__btnShoes');
+let sidenav__btnNewModels = document.querySelector('.sidenav__btnNewModels');
+let sidenav__btnDiscounts = document.querySelector('.sidenav__btnDiscounts');
 //_____________________________________________________________________________________________________________________
 
 //__________________________________Поиск товаров по категория_________________________________________________________
@@ -40,6 +50,7 @@ arrGoods(new_models);
 arrGoods(discount);
 //__________________________________Поиск кнопок из списка____________________________________________________________
 let allBtn = document.querySelectorAll('.category__text');
+let allSidenavBtn = document.querySelectorAll('.sidenav__category__text');
 //__________________________________Нахождение главной кнопки одежды_______________________________________________
 let btn__clothes = document.querySelector('.btn__clothes');
 //__________________________________Удаление display none из блока класса товара___________________________________
@@ -48,6 +59,9 @@ btn__clothes.addEventListener('click', function(e){
         i.classList.remove('dispNone');
         for(let iBtn of allBtn){
             iBtn.classList.remove('btnColor');
+        }
+        for(let sidenBtn of allSidenavBtn){
+            sidenBtn.classList.remove('btnColor');
         }
     }
 });
@@ -65,6 +79,20 @@ for(let btn of allBtn){
         ulBtn(btnShoes,   'goods__block shoes_men',   "new_models", "discount", 'dispNone', e);
         ulBtn(btnNewModels,'goods__block', "new_models", "discount", 'dispNone', e);
         ulBtn(btnDiscounts,'goods__block', "new_models", "discount", 'dispNone', e);
+
+    });
+}
+for(let sideBtn of allSidenavBtn){
+    sideBtn.addEventListener('click', function func(e){
+        ulBtn(sidenav__btnPants,   'goods__block pants_men',   "new_models", "discount", 'dispNone', e);
+        ulBtn(sidenav__btnJacket,  'goods__block jacket_men',  "new_models", "discount", 'dispNone', e);
+        ulBtn(sidenav__btnHoodies, 'goods__block hoodies_men', "new_models", "discount", 'dispNone', e);
+        ulBtn(sidenav__btnJeans,   'goods__block jeans_men',   "new_models", "discount", 'dispNone', e);
+        ulBtn(sidenav__btnHomclot, 'goods__block homclot_men', "new_models", "discount", 'dispNone', e);
+        ulBtn(sidenav__btnTshirts, 'goods__block tshirts_men', "new_models", "discount", 'dispNone', e);
+        ulBtn(sidenav__btnShoes,   'goods__block shoes_men',   "new_models", "discount", 'dispNone', e);
+        ulBtn(sidenav__btnNewModels,'goods__block', "new_models", "discount", 'dispNone', e);
+        ulBtn(sidenav__btnDiscounts,'goods__block', "new_models", "discount", 'dispNone', e);
     });
 }
 
@@ -84,14 +112,14 @@ function ulBtn(button, class1, class2, class3, classNone, e){
             }
         }
     }
-    if(e.currentTarget == button && button == btnNewModels){
+    if(e.currentTarget == button && button == btnNewModels  || e.currentTarget == button && button == sidenav__btnNewModels){
         for(let i of arr){
             if(i.classList.contains('new_models')){
                 i.classList.remove(classNone);
             }
         }
     }
-    if(e.currentTarget == button && button == btnDiscounts){
+    if(e.currentTarget == button && button == btnDiscounts  || e.currentTarget == button && button == sidenav__btnDiscounts){
         for(let i of arr){
             if(i.classList.contains('discount')){
                 i.classList.remove(classNone);
